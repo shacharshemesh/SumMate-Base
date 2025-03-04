a2b4a99084dcb7642629c8d5507ae29628f60707
nitzan <nitzan.h.2000@gmail.com>
2025-03-04 07:30:30 +0200

    fix ai connection

--- client/src/pages/Summarize.tsx
+++ client/src/pages/Summarize.tsx
@@ -4,11 +4,14 @@ import { improveTextWithAI } from "../services/AI";
 const Summarize = () => {
   const [AIErrors, setAIErrors] = useState<string | null>(null);
 
+  const [summery, setSummery] = useState<string>("");
+  const [improvedSammery, setImprovedSammery] = useState<string>("");
+
   const improveText = async () => {
     try {
-      const improvedText = await improveTextWithAI("");
+      const improvedText = await improveTextWithAI(summery);
+      setImprovedSammery(improvedText);
       setAIErrors(null);
-      console.log(improvedText)
     } catch (error) {
       if (error instanceof Error) {
         setAIErrors(`Error improving text. ${error.message}`);
@@ -28,14 +31,14 @@ const Summarize = () => {
             <h4 className="col-12">SumMate Assistent - co</h4>
           </div>
           <div className="row justify-content-center">
-            <textarea className="col-11" style={{ borderRadius: "20px", padding: "10px", height: "20vh" }}>fsjkj</textarea>
+            <textarea value={summery} onChange={(e) => setSummery(e.target.value)} placeholder="Enter your summery hear" className="col-11" style={{ borderRadius: "20px", padding: "10px", height: "20vh" }}>{summery}</textarea>
             {AIErrors && <p className="text-danger">{AIErrors}</p>}
           </div>
           <div className="row justify-content-center my-3">
             <button className="btn btn-primary col-6" onClick={improveText} style={{ borderRadius: "50px" }}>Improve with AI</button>
           </div>
           <div className="row justify-content-center">
-            <textarea className="col-11" style={{ borderRadius: "20px", padding: "10px", height: "20vh" }}>fsjkj</textarea>
+            <textarea value={improvedSammery} disabled className="col-11" style={{ borderRadius: "20px", padding: "10px", height: "20vh" }}></textarea>
           </div>
 
         </div>
